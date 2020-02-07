7f3563fa593c7fe47a6ee31b9f2ede0086f9e678
Jun0kada <jun.okada0203@gmail.com>
2020-02-07 17:34:40 +0900

    Fix mapping features

--- lib/limited_release.rb
+++ lib/limited_release.rb
@@ -8,14 +8,16 @@ require 'limited_release/railtie'
 
 module LimitedRelease
   def self.features
-    @features ||= Dir[::Rails.root.join('config', 'limited_releases', '**', '*.rb')].sort_by { |path| path.split('/').length }.map do |path|
+    @features ||= Dir[::Rails.root.join('config', 'limited_releases', '**', '*.rb')].sort_by { |path| path.split('/').length }.map { |path|
       name = path.gsub(/(.+limited_releases\/|\.rb\z)/, '').classify
 
       Object.send(:remove_const, name.split('::').first) if Object.const_defined?(name.split('::').first)
 
+      [path, name]
+    }.map { |path, name|
       load path
       name.constantize
-    end
+    }
   end
 
   def self.reload!
